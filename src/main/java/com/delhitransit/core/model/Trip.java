f3fb7e1bb1576770b3115c038fd92fd3e7573a1e
singaltanmay <tanmaysingal@icloud.com>
2020-07-19 02:35:42 +0530

    Added models for Shape and Trip

--- src/main/java/com/delhitransit/core/model/Trip.java
+++ src/main/java/com/delhitransit/core/model/Trip.java
@@ -1,5 +1,30 @@
 package com.delhitransit.core.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Trip {
+
+    /**
+     * Identifies the route that this trip is a part of.
+     */
+    @Getter
+    @Setter
+    private Route route;
+
+
+    /**
+     * Identifies a trip.
+     */
+    @Getter
+    @Setter
+    private long id;
+
+    /**
+     * Identifies a geospatial shape that describes the vehicle travel path for a trip.
+     */
+    @Getter
+    @Setter
+    private Shape shape;
 
 }

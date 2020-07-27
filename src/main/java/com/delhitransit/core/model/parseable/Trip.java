9c8462f2e77178b53f0ba67214c7e18c8e2c0dc8
singaltanmay <tanmaysingal@icloud.com>
2020-07-27 13:50:05 +0530

    Replaced if/else with switch in TripReader

--- src/main/java/com/delhitransit/core/model/parseable/Trip.java
+++ src/main/java/com/delhitransit/core/model/parseable/Trip.java
@@ -4,6 +4,8 @@
 
 package com.delhitransit.core.model.parseable;
 
+import lombok.Getter;
+
 /**
  * For more information see https://developers.google.com/transit/gtfs/reference/#tripstxt
  */
@@ -12,38 +14,29 @@ public class Trip {
     /**
      * Identifies the route that this trip is a part of.
      */
+    @Getter
     private int routeId;
 
     /**
      * Identifies a trip.
      */
+    @Getter
     private String tripId;
 
     /**
      * Identifies a geospatial shape that describes the vehicle travel path for a trip.
      */
+    @Getter
     private int shapeId;
-
-    public int getRouteId() {
-        return routeId;
-    }
 
     public Trip setRouteId(int routeId) {
         this.routeId = routeId;
         return this;
     }
 
-    public String getTripId() {
-        return tripId;
-    }
-
     public Trip setTripId(String tripId) {
         this.tripId = tripId;
         return this;
-    }
-
-    public int getShapeId() {
-        return shapeId;
     }
 
     public Trip setShapeId(int shapeId) {

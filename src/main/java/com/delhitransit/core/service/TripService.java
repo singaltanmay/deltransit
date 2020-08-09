0b5c19913e9520c7d95fbe3edabbb2d22ba6b758
admin12 <jgfgdtrs@gmail.com>
2020-08-09 18:15:05 +0530

    Created TripService which uses TripRepository
    Created TripService which uses TripRepository

---
+++ src/main/java/com/delhitransit/core/service/TripService.java
@@ -0,0 +1,56 @@
+/*
+ * @author Ankit Varshney
+ */
+
+package com.delhitransit.core.service;
+
+import com.delhitransit.core.model.entity.TripEntity;
+import com.delhitransit.core.model.parseable.Trip;
+import com.delhitransit.core.reader.TripReader;
+import com.delhitransit.core.repository.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TripService {
+
+    TripRepository tripRepository;
+
+    @Autowired
+    public TripService(TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
+
+    public List<TripEntity> getAllTrips() {
+        return tripRepository.findAll();
+    }
+
+    public void initializeUnlinkedDatabase() throws IOException {
+        List<TripEntity> tripEntities = parseCsvToEntityList();
+        insertTrips(tripEntities);
+    }
+
+    private List<TripEntity> parseCsvToEntityList() throws IOException {
+
+        List<TripEntity> tripEntities = new ArrayList<>();
+
+        List<Trip> trips = new TripReader().read();
+        for (Trip trip : trips) {
+            tripEntities.add(new TripEntity(trip));
+        }
+
+        return tripEntities;
+
+    }
+
+    private void insertTrips(List<TripEntity> tripEntities) {
+        if (tripEntities != null && tripEntities.size() > 0) {
+            tripRepository.saveAll(tripEntities);
+        }
+    }
+
+}

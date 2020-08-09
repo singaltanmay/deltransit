8eb89dc1f5dc637973730c4bf4602c88d20d7892
ankit <ankitv397@gmail.com>
2020-08-09 18:29:41 +0530

    Added TripService in Controllers for testing

--- src/main/java/com/delhitransit/core/controller/AdminController.java
+++ src/main/java/com/delhitransit/core/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.delhitransit.core.controller;
 
 import com.delhitransit.core.service.RouteService;
+import com.delhitransit.core.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +14,19 @@ import java.io.IOException;
 public class AdminController {
 
     private RouteService routeService;
-
+    private TripService tripService;
     @Autowired
     public AdminController(RouteService routeService) {
         this.routeService = routeService;
+        this.tripService = tripService;
     }
 
     @PostMapping("init/routes")
     public void initializeRoutesTable() throws IOException {
         routeService.initializeUnlinkedDatabase();
+    }
+    @PostMapping("init/trips")
+    public void initializeTripsTables() throws IOException {
+        tripService.initializeUnlinkedDatabase();
     }
 }
--- src/main/java/com/delhitransit/core/controller/DelhiTransitController.java
+++ src/main/java/com/delhitransit/core/controller/DelhiTransitController.java
@@ -1,8 +1,10 @@
 package com.delhitransit.core.controller;
 
 import com.delhitransit.core.model.entity.RouteEntity;
+import com.delhitransit.core.model.entity.TripEntity;
 import com.delhitransit.core.model.parseable.Route;
 import com.delhitransit.core.service.RouteService;
+import com.delhitransit.core.service.TripService;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +18,21 @@ import java.util.List;
 public class DelhiTransitController {
 
     private RouteService routeService;
+    private TripService tripService;
 
     @Autowired
     public DelhiTransitController(RouteService routeService) {
         this.routeService = routeService;
+        this.tripService = tripService;
     }
 
     @GetMapping("routes")
     public List<RouteEntity> getAllRoutes() {
         return routeService.getAllRoutes();
+    }
+
+    @GetMapping("trips")
+    public List<TripEntity> getAllTrips() {
+        return tripService.getAllTrips();
     }
 }

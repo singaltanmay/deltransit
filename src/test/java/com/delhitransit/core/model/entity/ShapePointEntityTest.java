8b53c916664fa7ac6182f2a4593b923780632d66
ankit <ankitv397@gmail.com>
2020-08-10 00:35:01 +0530

    Created test for testing ShapePointEntity

---
+++ src/test/java/com/delhitransit/core/model/entity/ShapePointEntityTest.java
@@ -0,0 +1,35 @@
+/*
+ * @author Ankit Varshney
+ */
+
+package com.delhitransit.core.model.entity;
+
+import com.delhitransit.core.model.parseable.ShapePoint;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ShapePointEntityTest {
+
+    @Test
+    void entityFromParaseableConstructorTest() {
+        int shapeId = 001;
+        Double latitude = 77.1108082069294;
+        Double longitude = 28.6251975188353;
+        int sequence = 1234;
+
+        ShapePoint shapePoint = new ShapePoint().setShapeId(shapeId)
+                                                .setLatitude(latitude)
+                                                .setLongitude(longitude)
+                                                .setSequence(sequence);
+
+        ShapePointEntity shapePointEntity = new ShapePointEntity(shapePoint);
+        assertEquals(shapeId, shapePointEntity.getShapeId());
+        assertEquals(latitude, shapePointEntity.getLatitude());
+        assertEquals(longitude, shapePointEntity.getLongitude());
+        assertEquals(sequence, shapePointEntity.getSequence());
+
+    }
+
+}

b65c14d7f7318e384aa69fe7c76104fab5d828c9
ankit <ankitv397@gmail.com>
2020-08-09 19:36:11 +0530

    Created ShapePointRepository

---
+++ src/main/java/com/delhitransit/core/repository/ShapePointRepository.java
@@ -0,0 +1,14 @@
+/*
+ * @author Ankit Varshney
+ */
+
+package com.delhitransit.core.repository;
+
+import com.delhitransit.core.model.entity.ShapePointEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShapePointRepository extends JpaRepository<ShapePointEntity, Long> {
+
+}

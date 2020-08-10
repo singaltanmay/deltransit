a9d80a5d515ea4779a87a587ca7d5e0fd23eda9e
ankit <ankitv397@gmail.com>
2020-08-10 23:57:44 +0530

    Created StopTimeEntityTest

---
+++ src/test/java/com/delhitransit/core/model/entity/StopTimeEntityTest.java
@@ -0,0 +1,31 @@
+/*
+ * @author Ankit Varshney
+ */
+
+package com.delhitransit.core.model.entity;
+
+import com.delhitransit.core.model.parseable.StopTime;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StopTimeEntityTest {
+
+    @Test
+    void entityFromParseableConstructorTest() {
+        String arrival = "15:15:00";
+        String departure = "22:30:00";
+        int stopSequence = 4612;
+
+        StopTime stopTime = new StopTime().setArrival(arrival)
+                .setDeparture(departure)
+                .setStopSequence(stopSequence);
+
+        StopTimeEntity stopTimeEntity = new StopTimeEntity(stopTime);
+        assertEquals(arrival, stopTime.getArrival());
+        assertEquals(departure, stopTime.getDeparture());
+        assertEquals(stopSequence, stopTime.getStopSequence());
+    }
+
+}

9170cadc837d6ed381ac39e2c3f1ead3478eee00
singaltanmay <tanmaysingal@icloud.com>
2020-08-23 22:40:58 +0530

    .

--- src/test/java/com/delhitransit/core/CoreApplicationTests.java
+++ src/test/java/com/delhitransit/core/CoreApplicationTests.java
@@ -6,9 +6,7 @@ package com.delhitransit.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class CoreApplicationTests {
 

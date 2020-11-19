cf7352e88343977efded545c8ed77e30b48a91f0
Tanmay Singal <tanmaysingal@icloud.com>
2020-11-19 17:29:20 +0530

    Added endpoint to change URL of OTD-Parser

--- src/main/java/com/delhitransit/core/api/controller/AdminController.java
+++ src/main/java/com/delhitransit/core/api/controller/AdminController.java
@@ -1,12 +1,14 @@
 package com.delhitransit.core.api.controller;
 
 import com.delhitransit.core.service.InitializerService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("v1/admin")
@@ -19,9 +21,13 @@ public class AdminController {
         this.initializerService = initializerService;
     }
 
+    @ApiOperation(value = "Provide the base url of the OTD Parser API. For example :- \"https://otd-parser.herokuapp" +
+            ".com/v1/\". This endpoint should not be used if the database has already been partially initialized by " +
+            "another URL or default value.")
     @PostMapping("init")
-    public void initializeDatabase() throws IOException {
-        initializerService.init();
+    public void initializeDatabase(
+            @RequestParam(name = "otd", required = false, defaultValue = "https://otd-parser.herokuapp.com/v1/") Optional<String> otdUrl) {
+        initializerService.init(otdUrl);
     }
 
 }

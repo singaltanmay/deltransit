af49fd5a15f03d79c361c5f1221799289583c27c
Ankit <choutmp+drmfn@gmail.com>
2020-11-19 19:17:36 +0530

    Now test are woking fine
    Now test are woking fine

--- src/test/java/com/delhitransit/core/CoreApplicationTests.java
+++ src/test/java/com/delhitransit/core/CoreApplicationTests.java
@@ -6,8 +6,10 @@ package com.delhitransit.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoreApplicationTests {
 
     @Test

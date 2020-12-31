3c52acbcad9fc4066c2920bbd800eb2f9c336990
Tanmay Singal <tanmaysingal@icloud.com>
2020-12-31 20:00:19 +0530

    Logging every query when it starts and ends

--- src/main/java/com/delhitransit/core/api/swagger/SwaggerInterceptAdapter.java
+++ src/main/java/com/delhitransit/core/api/swagger/SwaggerInterceptAdapter.java
@@ -4,7 +4,11 @@
 
 package com.delhitransit.core.api.swagger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class SwaggerInterceptAdapter extends HandlerInterceptorAdapter {
 
+    private final Logger logger = LoggerFactory.getLogger(SwaggerInterceptAdapter.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
         String req = request.getRequestURI();
+
+        //Logging the request
+        logger.info("Received new request: " + request.getRequestURL());
 
         if ((req == null) || req.isEmpty() || req.equals("/") || req.equals("/v1") || req
                 .equals("/v1/")) {
@@ -27,4 +36,12 @@ public class SwaggerInterceptAdapter extends HandlerInterceptorAdapter {
         return super.preHandle(request, response, handler);
     }
 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        int status = response.getStatus();
+        //Logging the response status
+        boolean isRequestSuccessful = HttpStatus.valueOf(status) == HttpStatus.OK;
+        logger.info("Request " + (isRequestSuccessful ? "successful" : "failed") + ": " + request.getRequestURL());
+    }
 }
